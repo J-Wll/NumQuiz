67d9adfea6652eb55d490d5342dc353cee5e8b67
J-Wll <80954812+J-Wll@users.noreply.github.com>
2023-10-05 10:30:03 +0000

    Difficulty selection implemented

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <ctype.h>
 #include <ctime>
+#include <limits>
 
+using namespace std;
+
+void invalidInput(){
+  cout << "\n\033[1;31mInvalid input, please follow input requirements\033[0m\n\n"; 
+  cin.clear();
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
 
 // Prompts for and takes input of a char representing difficulty (Easy, Moderate, Advanced)
 char displayMenu(){
+  char dif;
+  cout << "Welcome to NumQuiz\n\nEnter your desired difficulty (Please enter E, M or A):\nE) Easy\nM) Moderate\nA) Advanced\n";
+  cin >> dif;
+  dif = toupper(dif);
+  if(dif == 'E' || dif == 'M' || dif == 'A'){
+    return dif;
+  } else {
+    invalidInput();    
+    return displayMenu();
+  } 
 }
 
 // Returns a random integer between two arguments
@@ -34,5 +53,7 @@ void displayFinalResults(){
 }
 
 int main() {
-  char difficulty = displayMenu();  
+  const char difficulty = displayMenu();  
+  cout << difficulty;
+  
 }
