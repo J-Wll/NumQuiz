99733e0e7df876e8b6ce8d20de2060d7a750ab7b
J-Wll <80954812+J-Wll@users.noreply.github.com>
2023-10-05 10:02:40 +0000

    Established initial functions according to the brief

--- .ccls-cache/@home@runner@NumQuiz/main.cpp
+++ .ccls-cache/@home@runner@NumQuiz/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <ctime>
 
-int main() {
+
+// Prompts for and takes input of a char representing difficulty (Easy, Moderate, Advanced)
+char displayMenu(){
+}
+
+// Returns a random integer between two arguments
+int randomInt(int min, int max){
+}
+
+// Prompts and takes user input of difficulty
+void generateOperands(char difficulty){
   
 }
 
+// Prompts and takes user input of answer
+long int displayProblem(int operand1, int operand2){
+  
+}
+
+// Returns true/false depending on answer
+bool isCorrect(int operand1, int operand2, int answer){
+  
+}
+
+void displayMessage(){
+  // Outputs correct or incorrect, and displays if the user has another chance
+}
+
+void displayFinalResults(){
+  // Tally of correct and incorrect answers
+}
+
+int main() {
+  char difficulty = displayMenu();  
+}
